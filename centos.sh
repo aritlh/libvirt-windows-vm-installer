f443428e1b9203ef6c861db71375fee7556005d1
Ari <a.arijogja@gmail.com>
2024-11-21 18:06:17 +0700

    fix: delete size from --disk parameter

--- centos.sh
+++ centos.sh
@@ -110,7 +110,7 @@ echo "Creating and starting VM..."
 sudo virt-install \
   --name windows_server_2012 \
   --ram 45000 \
-  --disk path=/var/lib/libvirt/images/windows_server_2012.qcow2,size=2048 \
+  --disk path=/var/lib/libvirt/images/windows_server_2012.qcow2 \
   --vcpus 12 \
   --noautoconsole \
   --os-variant win2k22 \
